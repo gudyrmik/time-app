45b1fa480394c4c693bbc4489aa4b6f4a49f0ba3
Mikhail Gudyrin <gudyrmik@gmail.com>
2020-08-14 13:55:42 +0200

    3

--- time_formatter.rb
+++ time_formatter.rb
@@ -1,24 +1,23 @@
 class TimeFormatter
   ACCEPTABLE_FORMATS = ["year", "month", "day", "hour", "minute", "second"].freeze
   STRFTIME_FORMATS = ["%Y", "%m", "%d", "%H", "%M", "%S"].freeze
-  HEADER = { 'Content-Type' => 'text/plain' }.freeze
 
-  attr_reader :result
+  attr_reader :response
 
   def initialize(request_path, query_string)
-    @result = parse_request(request_path, query_string)
+    @response = parse(request_path, query_string)
   end
 
-  private
+  def valid?
+    @response.include?('Unknown') == false && @response.nil? == false
+  end
 
-  def parse_request(request_path, query_string)
-    return [404, HEADER, ['Unknown URL']] unless request_path == '/time'
-    query_hash = Rack::Utils.parse_nested_query(query_string)
-    return [404, HEADER, ['Unknown param']] if query_hash['format'].nil?
+  private 
 
+  def parse(request_path, query_string)
     body_ok = ""
     body_failed = ""
-    queried_formats = query_hash['format'].split(',')
+    queried_formats = Rack::Utils.parse_nested_query(query_string)['format'].split(',')
     
     queried_formats.each_with_index do |time_format, index|
       if ACCEPTABLE_FORMATS.include?(time_format)
@@ -31,9 +30,9 @@ class TimeFormatter
     end
 
     if body_failed.empty?
-      [200, HEADER, [Time.now.strftime(body_ok)]]
+      Time.now.strftime(body_ok)
     else
-      [200, HEADER, ["Unknown time format [#{body_failed}]" ]]
+      "Unknown time format [#{body_failed}]"
     end
   end
 end
